6c9f762a20cfd686cba14f4f16268e439ea351e7
gaosongling <gaosongling@xiaomi.com>
2018-10-30 00:12:04 +0800

    完善了时间戳工具类

--- 01-javase/javase-01-base/src/main/java/cn/gaohank/program/javase/base/base_11_utils/Utils01Time.java
+++ 01-javase/javase-01-base/src/main/java/cn/gaohank/program/javase/base/base_11_utils/Utils01Time.java
@@ -1,26 +1,29 @@
 package cn.gaohank.program.javase.base.base_11_utils;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Utils01Time {
-    public static void main(String[] args) {
-        long currentTimeMillis = getCurrentTimeMillis();
-        System.out.println(currentTimeMillis);
-
-        Date curDate = new Date();
-        System.out.println(curDate);
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        System.out.println(simpleDateFormat.format(curDate));
-//        System.out.println(simpleDateFormat.parse("2009/09/09 09:09:09"));
-    }
-
-    public static long getCurrentTimeMillis() {
+    // 获得当前的系统时间，精确到毫秒
+    public long getCurrentTimeMillis() {
         return System.currentTimeMillis();
     }
 
+    // 将Date转化成unix时间戳
+    public long getUnixTimestamp(Date date) {
+        return date.getTime();
+    }
+
+    // 将Date格式化成String的时间
     public String getFormatDate(Date date, String parse) {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(parse);
         return simpleDateFormat.format(date);
+    }
+
+    // 将String的时间格式化成Date
+    public Date getParseDate(String date, String parse) throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(parse);
+        return simpleDateFormat.parse(date);
     }
 }
--- 01-javase/javase-01-base/src/test/java/cn/gaohank/program/javase/base/base_11_utils/Utils01TimeTest.java
+++ 01-javase/javase-01-base/src/test/java/cn/gaohank/program/javase/base/base_11_utils/Utils01TimeTest.java
@@ -4,13 +4,34 @@ import org.junit.Test;
 
 import java.util.Date;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class Utils01TimeTest {
+    private final static Utils01Time utils01Time = new Utils01Time();
+
+    @Test
+    public void getCurrentTimeMillis() {
+        System.out.println(utils01Time.getCurrentTimeMillis());
+    }
+
+    @Test
+    public void getUnixTimestamp() throws Exception {
+        // 2011/11/11 11:11:11 转换成unix时间戳是：1320981071000 单位是ms
+        assertEquals(1320981071000L, utils01Time.getUnixTimestamp(
+                utils01Time.getParseDate("2011-11-11 11:11:11", "yyyy-MM-dd HH:mm:ss")
+        ));
+    }
 
     @Test
     public void getFormatDate() {
-        Utils01Time utils01Time = new Utils01Time();
-        System.out.println(utils01Time.getFormatDate(new Date(), "yyyyMMdd"));
+        // 2011/11/11 11:11:11 转换成unix时间戳是：1320981071000 单位是ms
+        assertEquals("20111111", utils01Time.getFormatDate(new Date(1320981071000L), "yyyyMMdd"));
+    }
+
+    @Test
+    public void getParseDate() throws Exception {
+        String dateStr = "2018-10-10 10:10:10";
+        Date parseDate = utils01Time.getParseDate(dateStr, "yyyy-MM-dd HH:mm:ss");
+        assertEquals("20181010", utils01Time.getFormatDate(parseDate, "yyyyMMdd"));
     }
 }
