b18b508f5c17850a43895de76a659e64d0e95d13
gaosongling <gaosongling@xiaomi.com>
2018-11-04 20:22:17 +0800

    增加了Queue和栈

--- 01-javase/javase-01-base/src/main/java/cn/gaohank/program/javase/base/base_05_collection/list/List03Queue.java
+++ 01-javase/javase-01-base/src/main/java/cn/gaohank/program/javase/base/base_05_collection/list/List03Queue.java
@@ -1,4 +1,27 @@
 package cn.gaohank.program.javase.base.base_05_collection.list;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+// 队列由LinkedList实现
 public class List03Queue {
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<String>();
+        queue.offer("hank");
+        queue.offer("Bob");
+        queue.offer("james");
+        queue.offer("hank");
+        queue.offer(null);
+        queue.offer("james");
+
+
+        System.out.println(queue.peek());
+        System.out.println(queue.peek()); // 取队首元素，不删除
+        System.out.println(queue.poll()); // 删除队首元素并返回该元素
+        System.out.println("-------------");
+
+        for (String str : queue) {
+            System.out.println(str);
+        }
+    }
 }
--- 01-javase/javase-01-base/src/main/java/cn/gaohank/program/javase/base/base_05_collection/list/List04Deque.java
+++ 01-javase/javase-01-base/src/main/java/cn/gaohank/program/javase/base/base_05_collection/list/List04Deque.java
@@ -1,4 +1,29 @@
 package cn.gaohank.program.javase.base.base_05_collection.list;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
+// 栈由LinkedList实现
 public class List04Deque {
+    public static void main(String[] args) {
+        Deque<String> deque = new LinkedList<String>();
+        deque.push("aaa");
+        deque.push("bbb");
+        deque.push("ccc");
+        deque.push(null);
+        deque.push("bbb");
+        deque.push("ccc");
+
+        for (String str : deque) {
+            System.out.println(str);
+        }
+
+        System.out.println(deque.size());
+
+        deque.forEach((s)->System.out.println(s + "---" + deque.size()));
+        System.out.println(deque.pop());
+        System.out.println(deque.pop());
+        System.out.println(deque.pop());
+        System.out.println(deque);
+    }
 }
