6868f3bfef74f7c2d7891da0602347585572ce9f
gaosongling <gaosongling@xiaomi.com>
2018-11-06 01:00:02 +0800

    增加了IOUtils中的Scanner方法

--- 01-javase/javase-01-base/src/main/java/cn/gaohank/program/javase/base/base_07_io/Io10IoUtils.java
+++ 01-javase/javase-01-base/src/main/java/cn/gaohank/program/javase/base/base_07_io/Io10IoUtils.java
@@ -3,8 +3,18 @@ package cn.gaohank.program.javase.base.base_07_io;
 
 import org.apache.commons.io.IOUtils;
 
+import java.util.Scanner;
+
 public class Io10IoUtils {
     public static String getContent(String resourceName) throws Exception {
         return IOUtils.toString(Io10IoUtils.class.getClassLoader().getResourceAsStream(resourceName), "utf8");
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(getContent("test.json"));
+
+        while (scanner.hasNextLine()) {
+            System.out.println(scanner.nextLine());
+        }
     }
 }
