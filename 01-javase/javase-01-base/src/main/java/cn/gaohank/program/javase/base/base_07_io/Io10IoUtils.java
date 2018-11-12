1f1477c0b73efd5178c0927b3f811c53d0d17530
gaosongling <gaosongling@xiaomi.com>
2018-11-12 08:41:33 +0800

    更新了ConfigFactory读取配置文件

--- 01-javase/javase-01-base/src/main/java/cn/gaohank/program/javase/base/base_07_io/Io10IoUtils.java
+++ 01-javase/javase-01-base/src/main/java/cn/gaohank/program/javase/base/base_07_io/Io10IoUtils.java
@@ -1,13 +1,24 @@
 package cn.gaohank.program.javase.base.base_07_io;
 
 
+import com.typesafe.config.ConfigFactory;
 import org.apache.commons.io.IOUtils;
 
+import java.io.File;
+import java.util.List;
 import java.util.Scanner;
 
 public class Io10IoUtils {
     public static String getContent(String resourceName) throws Exception {
         return IOUtils.toString(Io10IoUtils.class.getClassLoader().getResourceAsStream(resourceName), "utf8");
+    }
+
+    public static List<String> getQueries(File file, String path) {
+        return ConfigFactory.parseFile(file).getStringList(path);
+    }
+
+    public static List<String> getQueries(String conf, String path) {
+        return ConfigFactory.load(conf).getStringList(path);
     }
 
     public static void main(String[] args) throws Exception {
@@ -16,5 +27,12 @@ public class Io10IoUtils {
         while (scanner.hasNextLine()) {
             System.out.println(scanner.nextLine());
         }
+
+        String name = ConfigFactory.load("query.conf")
+                .getString("cn.gaohank.program.javase.base.student.hank.name");
+        System.out.println(name);
+
+        System.out.println(getQueries("query.conf",
+                "cn.gaohank.program.javase.base.student.hank.queries"));
     }
 }
