b0c513ec058eca0137c62ac822af690a2b012356
gaosongling <gaosongling@xiaomi.com>
2018-11-17 14:56:28 +0800

    LinkedHashSet

--- 01-javase/javase-01-base/src/main/java/cn/gaohank/program/javase/base/base_05_collection/set/Set02LinkedHashSet.java
+++ 01-javase/javase-01-base/src/main/java/cn/gaohank/program/javase/base/base_05_collection/set/Set02LinkedHashSet.java
@@ -1,4 +1,25 @@
 package cn.gaohank.program.javase.base.base_05_collection.set;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/*
+ * LinkedHashSet
+ * 按照add的顺序组成集合
+ * 不重复
+ * 可以有null
+ */
 public class Set02LinkedHashSet {
+    public static void main(String[] args) {
+        Set<String> set = new LinkedHashSet<String>();
+
+        set.add("java");
+        set.add("c");
+        set.add("c++");
+        set.add("c");
+        set.add(null);
+        set.add("c");
+        set.add(".net");
+        System.out.println(set);
+    }
 }
