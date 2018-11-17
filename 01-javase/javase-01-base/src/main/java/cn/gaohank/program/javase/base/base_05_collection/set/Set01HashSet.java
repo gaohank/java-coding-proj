49fb8d0addf87c09011b68cd7d7c4ada10554ef4
gaosongling <gaosongling@xiaomi.com>
2018-11-17 14:53:32 +0800

    增加了Set

--- 01-javase/javase-01-base/src/main/java/cn/gaohank/program/javase/base/base_05_collection/set/Set01HashSet.java
+++ 01-javase/javase-01-base/src/main/java/cn/gaohank/program/javase/base/base_05_collection/set/Set01HashSet.java
@@ -1,4 +1,54 @@
 package cn.gaohank.program.javase.base.base_05_collection.set;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Set01HashSet {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<String>();
+
+        // 1.add
+        System.out.println("====1.add====");
+        set.add("java");
+        set.add("c");
+        set.add("c++");
+        set.add("c");
+        set.add("c");
+        set.add(".net");
+        System.out.println(set);
+
+        // 2.remove
+        System.out.println("====2.remove====");
+        set.remove("c");
+        System.out.println(set);
+
+        // 3.toArray
+        System.out.println("====3.toArray====");
+        String str[] = set.toArray(new String[set.size()]);
+        System.out.println(Arrays.toString(str));
+
+        // 4.for
+        System.out.println("====4.for====");
+        for (String str1:set) {
+            System.out.print(str1+"\t");
+        }
+        System.out.println();
+
+        // 5.iterator
+        System.out.println("====5.iterator====");
+        Iterator<String> it = set.iterator();
+        while (it.hasNext()) {
+            System.out.print(it.next() + "\t");
+        }
+        System.out.println();
+
+        // 6.stream
+        System.out.println("====6.stream====");
+        // 引用构造器LinkedHashSet::new相当于(a,b)->new LinkedHashSet(a,b)
+        System.out.println(set.stream().sorted(String::compareTo).
+                collect(Collectors.toList()));
+    }
 }
--- 01-javase/javase-01-base/src/main/java/cn/gaohank/program/javase/base/base_05_collection/set/Set03TreeSet.java
+++ 01-javase/javase-01-base/src/main/java/cn/gaohank/program/javase/base/base_05_collection/set/Set03TreeSet.java
@@ -1,4 +1,51 @@
 package cn.gaohank.program.javase.base.base_05_collection.set;
 
+import java.util.Set;
+import java.util.TreeSet;
+
+/*
+ * TreeSet
+ * 有序，不重复，不能有null
+ * 装入的程序必须实现Comparable接口
+ */
 public class Set03TreeSet {
+    private static class Personal implements Comparable<Personal> {
+        String name;
+        int age;
+        int score;
+
+        public Personal(String name, int age, int score) {
+            this.name = name;
+            this.age = age;
+            this.score = score;
+        }
+
+        @Override
+        public int compareTo(Personal o) {
+            return this.age - o.age;
+        }
+
+        @Override
+        public String toString() {
+            return "[name=" + name + ",age=" + age + ",score=" + score + "]";
+        }
+    }
+
+    public static void main(String[] args) {
+        Set<Integer> set = new TreeSet<>();
+
+        set.add(4);
+        set.add(3);
+        set.add(5);
+
+        System.out.println(set);
+
+        Set<Personal> personalSet = new TreeSet<>();
+
+        personalSet.add(new Personal("hank", 28, 90));
+        personalSet.add(new Personal("james", 32, 97));
+        personalSet.add(new Personal("vencent", 19, 82));
+
+        System.out.println(personalSet);
+    }
 }
