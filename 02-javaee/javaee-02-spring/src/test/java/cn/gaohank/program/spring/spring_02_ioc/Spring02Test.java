03da857bdc7b10d0eec6e9ea1aba03fe2b42ab43
gaosongling <gaosongling@xiaomi.com>
2019-01-02 15:43:45 +0800

    修改程序

--- 02-javaee/javaee-02-spring/src/test/java/cn/gaohank/program/spring/spring_02_ioc/Spring02Test.java
+++ 02-javaee/javaee-02-spring/src/test/java/cn/gaohank/program/spring/spring_02_ioc/Spring02Test.java
@@ -8,44 +8,47 @@ public class Spring02Test {
     /**
      * 测试IOC
      */
-//    @Test
-//    public void test0() {
-//    	//将配置文件导入到上下文中
-//        String cfg = "applicationContext.xml";
-//        ApplicationContext ctx =
-//            new ClassPathXmlApplicationContext(cfg);
-//        //通过spring创建实例
-//        MessageBean bean = ctx.getBean("msg", MessageBean.class);
-//        bean.execute();
-//    }
-//
+    @Test
+    public void test0() {
+    	//将配置文件导入到上下文中
+        String cfg = "applicationContext-spring-02.xml";
+        ApplicationContext ctx =
+            new ClassPathXmlApplicationContext(cfg);
+        //通过spring创建实例
+        MessageBean bean = ctx.getBean("msg", MessageBean.class);
+        bean.execute();
+    }
+
     /**
      * 注入参数值
      */
-//    @Test
-//    public void test1() {
-//        String cfg = "applicationContext.xml";
-//        ApplicationContext ctx =
-//            new ClassPathXmlApplicationContext(cfg);
-//        MessageBean bean = ctx.getBean("msg", MessageBean.class);
-//        bean.execute();
-//    }
+    @Test
+    public void test1() {
+        String cfg = "applicationContext-spring-02.xml";
+        ApplicationContext ctx =
+            new ClassPathXmlApplicationContext(cfg);
+        MessageBean bean = ctx.getBean("msg", MessageBean.class);
+        bean.execute();
+    }
 
-//    /**
-//     * 采用引用的方式注入集合
-//     */
-//    @Test
-//    public void test2() {
-//        String cfg = "applicationContext.xml";
-//        ApplicationContext ctx =
-//            new ClassPathXmlApplicationContext(cfg);
-//        MessageBean bean = ctx.getBean("msg2", MessageBean.class);
-//        bean.execute();
-//    }
-//
-//    /**
-//     * 注入表达式
-//     */
+    /**
+     * 采用引用的方式注入集合
+     */
+    @Test
+    public void test2() {
+        String cfg = "applicationContext-spring-02.xml";
+        ApplicationContext ctx =
+            new ClassPathXmlApplicationContext(cfg);
+        MessageBean bean = ctx.getBean("msg2", MessageBean.class);
+        MessageBean bean2 = ctx.getBean("msg2", MessageBean.class);
+        bean.execute();
+        // 使用的是同一个score对象
+        System.out.println(bean.getScore() == bean2.getScore());
+    }
+
+    /**
+     * 注入表达式
+     */
     @Test
     public void test3() {
         String cfg = "applicationContext-spring-02.xml";
