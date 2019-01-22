5a07da7f4edaea174b972b22af2809d531b14b58
gaosongling <gaosongling@xiaomi.com>
2019-01-23 00:50:36 +0800

    增加paoding的项目

---
+++ 02-javaee/javaee-05-paoding/src/main/java/cn/gaohank/program/paoding/controller/HelloController.java
@@ -0,0 +1,12 @@
+package cn.gaohank.program.paoding.controller;
+
+import net.paoding.rose.web.annotation.Path;
+import net.paoding.rose.web.annotation.rest.Get;
+
+@Path("")
+public class HelloController {
+    @Get("")
+    public String index() {
+        return "@hello world";
+    }
+}
