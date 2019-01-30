05250cfe30ba3459189846ba069e8663bd34c392
gaosongling <gaosongling@xiaomi.com>
2019-01-30 10:32:01 +0800

    增加了id

--- 02-javaee/javaee-04-springboot/springboot-02-paoding/src/main/java/cn/gaohank/program/module/SleepMonitor.java
+++ 02-javaee/javaee-04-springboot/springboot-02-paoding/src/main/java/cn/gaohank/program/module/SleepMonitor.java
@@ -1,10 +1,12 @@
 package cn.gaohank.program.module;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="SleepMonitor")
 @Data
 public class SleepMonitor {
+    @Id
     private String _id;
 }
